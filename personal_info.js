62ae65ee1e21ffd501829937e0d18a4a061aea6e
Noe <noeliapaz676@gmail.com>
2022-08-12 14:27:47 -0300

    delete files.

---
+++ personal_info.js
@@ -0,0 +1,12 @@
+const info = [
+  'Noelia',
+  29,
+  true,
+  new Date(1992, 11, 30),
+  {
+    title: 'El hombre mas feliz del mundo',
+    author: 'Eddie jaku',
+    date: new Date(1938, 0, 1),
+    url: 'https://www.amazon.es/dp/B09BKDN2DF/',
+  },
+];
