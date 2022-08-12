3decbe481b4a583b0dfce0f8877516b7e6c3d4d4
Noe <noeliapaz676@gmail.com>
2022-08-12 12:28:13 -0300

    conceptos.

---
+++ variables-y-constantes.js
@@ -0,0 +1,24 @@
+// una variable es una especie de caja donde se almacena un valor y despues lo podemos cambiar.
+// var afecta a todo el codigo osea si cambias algo se cambia en todo.
+// let afecta solo al bloque donde este siendo declarado.
+var variables;
+let variableLet;
+
+var a = 1;
+console.log(a);
+a = 4;
+console.log(a);
+
+let b = 3;
+console.log(b);
+b = 5;
+console.log(b);
+
+//const constante estas no se pueden cambiar.
+const constante = 'hola soy una constante';
+console.log(constante);
+//constante = "adios" no se puede da error
+
+// typeof nos dice que tipo de variable es o que tipo primitivo de algo
+console.log(typeof 1);
+console.log(typeof '1');
