4a7735c90b47193ff5f271c0339c56ba7cd95cc7
Noe <noeliapaz676@gmail.com>
2022-08-18 09:52:02 -0300

    Create text.

---
+++ cadenasTextos.js
@@ -0,0 +1,10 @@
+const nombre = 'noe';
+const apellido = 'paz';
+const estudiante = nombre.concat(' ').concat(apellido);
+const estudianteMayus = estudiante.toUpperCase();
+const estudianteMinus = estudiante.toLowerCase();
+const estudianteLength = estudiante.length;
+const inicialNombre = nombre.charAt(0);
+const finalApellido = apellido.charAt(2);
+const estudianteSinEspacios = estudiante.trim().length;
+const nombreEnEstudiante = estudiante.includes(nombre);
