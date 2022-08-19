4a7246b527f64707e55e3ba21a6da19e3610526c
Noe <noeliapaz676@gmail.com>
2022-08-19 10:44:01 -0300

    create number.

---
+++ numeros.js
@@ -0,0 +1,6 @@
+const altura_cm = 164;
+const altura_m = 1.64;
+const peso_kg = 58.6;
+const altura_redondeada = Math.ceil(altura_m);
+const peso_redond = Math.floor(peso_kg);
+const sonIguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
